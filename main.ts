f32540936f1d273ca419fb31eb4da1fee7e9c5d1
Aitana <aitana.buiza@gracia.lasalle.cat>
2025-12-03 11:18:59 +0100

    add show message

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("Hello world")
 })
 radio.onReceivedString(function (receivedString) {
-	
+    basic.showString(receivedString)
 })
 radio.setGroup(1)
